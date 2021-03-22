90d95afda4221f0df5de6f7cd9d84c52aa9d4e9f
Arun Subash Manickam <arunsubashm@gmail.com>
2021-03-23 05:17:59 +0530

    Define an Entity object for Customer-Address Table which joins the Customer and the Address table

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CustomerAddressEntity.java
@@ -0,0 +1,49 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "customer_address", schema = "public")
+@NamedQueries({
+
+})
+
+public class CustomerAddressEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // This assumes a customer can have more than one address
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private CustomerEntity customer;
+
+    // One ID for a given address
+    @OneToOne
+    @JoinColumn(name = "address_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private char address; // Change it to Address Entity once defined
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public CustomerEntity getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerEntity customer) {
+        this.customer = customer;
+    }
+}
