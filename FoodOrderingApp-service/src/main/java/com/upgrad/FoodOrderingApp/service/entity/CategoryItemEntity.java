c1765e3b8499d251386dc2c22f97908936d553b8
Arun Subash Manickam <arunsubashm@gmail.com>
2021-03-23 05:38:48 +0530

    Implement the Category and Category Item Entities

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryItemEntity.java
@@ -0,0 +1,46 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category_item", schema = "public")
+@NamedQueries({
+
+})
+public class CategoryItemEntity {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    // One item can be in many categories
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private char item; // Replace with Item Entity once defined
+
+    // One category can have many items
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private CategoryEntity category;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public CategoryEntity getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEntity category) {
+        this.category = category;
+    }
+}
