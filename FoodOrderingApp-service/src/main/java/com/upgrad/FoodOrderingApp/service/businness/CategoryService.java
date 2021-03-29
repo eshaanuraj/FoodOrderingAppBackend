76c5b66e980659c8c55124e74626ffa40a9ef199
Rajkumar Lolla <rajkumarlolla@yahoo.com>
2021-03-29 21:15:55 +0530

    Fixed Errors

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -1,18 +1,14 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
-import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
-import com.upgrad.FoodOrderingApp.service.dao.CategoryItemDao;
-import com.upgrad.FoodOrderingApp.service.dao.ItemDao;
-import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
-import com.upgrad.FoodOrderingApp.service.entity.CategoryItemEntity;
-import com.upgrad.FoodOrderingApp.service.entity.ItemEntity;
+import com.upgrad.FoodOrderingApp.service.dao.*;
+import com.upgrad.FoodOrderingApp.service.entity.*;
 import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
-import com.upgrad.FoodOrderingApp.service.exception.UpdateCustomerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Service
@@ -26,6 +22,11 @@ public class CategoryService {
 
     @Autowired
     private ItemDao itemDao;
+    @Autowired
+    private RestaurantDao restaurantDao;
+
+    @Autowired
+    private RestaurantCategoryDao restaurantCategoryDao;
 
     // Service to get all category Records
     @Transactional
@@ -70,5 +71,21 @@ public class CategoryService {
             itemEntityList.add(categoryItemEntity.getItem());
         }
         return itemEntityList;
+    }
+
+    public List<CategoryEntity> getCategoriesByRestaurant(String restaurantUuid){
+
+        //Calls getRestaurantByUuid of restaurantDao to get RestaurantEntity
+        RestaurantEntity restaurantEntity = restaurantDao.getRestaurantByUuid(restaurantUuid);
+
+        //Calls getCategoriesByRestaurant of restaurantCategoryDao to get list of RestaurantCategoryEntity
+        List<RestaurantCategoryEntity> restaurantCategoryEntities = restaurantCategoryDao.getCategoriesByRestaurant(restaurantEntity);
+
+        //Creating the list of the Category entity to be returned.
+        List<CategoryEntity> categoryEntities = new LinkedList<>();
+        restaurantCategoryEntities.forEach(restaurantCategoryEntity -> {
+            categoryEntities.add(restaurantCategoryEntity.getCategory());
+        });
+        return categoryEntities;
     }
 }
